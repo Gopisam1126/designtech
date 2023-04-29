9e489293ba706ef9795a5751d8b4297144c512e8
Scriptkid <lifeisawsomeenjoy@gmail.com>
2023-04-29 11:13:51 +0530

    commit1

---
+++ index.js
@@ -0,0 +1,8 @@
+function navBar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar"
+    }
+}
